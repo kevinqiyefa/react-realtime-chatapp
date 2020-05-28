659feede739100a9aa73a7e714465b3caf7f6087
Kevin <kevinqiyf@hotmail.com>
2020-05-27 18:55:03 -0700

    add styles to inputform

--- client/src/components/InputForm/index.jsx
+++ client/src/components/InputForm/index.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './InputForm.module.css';
 
 const InputForm = ({ message, setMessage, sendMessage }) => {
   return (
-    <form className="form" onSubmit={(e) => sendMessage(e)}>
+    <form className={styles.inputForm} onSubmit={(e) => sendMessage(e)}>
       <input
-        className="input"
+        className={styles.input}
         type="text"
         placeholder="Type a message..."
         value={message}
         onChange={({ target: { value } }) => setMessage(value)}
       />
-      <button className="sendButton" type="submit">
+      <button className={styles.sendButton} type="submit">
         Send
       </button>
     </form>
