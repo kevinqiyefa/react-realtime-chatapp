84faddd86e55ee8d8f159328954f0ab1ce3003c8
Kevin <kevinqiyf@hotmail.com>
2020-05-28 17:54:42 -0700

    finish adding all styles

--- client/src/components/TextContainer/index.jsx
+++ client/src/components/TextContainer/index.jsx
@@ -29,16 +29,15 @@ const TextContainer = ({ users }) => (
     {users ? (
       <div>
         <h1>People currently chatting:</h1>
-        <div className="activeContainer">
-          <h2>
-            {users.map(({ name }) => (
-              <div key={name} className="activeItem">
-                {name}
-                <img alt="Online Icon" src={onlineIcon} />
-              </div>
-            ))}
-          </h2>
-        </div>
+
+        <h2>
+          {users.map(({ name }) => (
+            <div key={name} className={styles.activeItem}>
+              {name}
+              <img alt="Online Icon" src={onlineIcon} />
+            </div>
+          ))}
+        </h2>
       </div>
     ) : null}
   </div>
